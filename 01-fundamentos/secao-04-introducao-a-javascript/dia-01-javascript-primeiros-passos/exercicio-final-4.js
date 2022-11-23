ce530359b0ed3d23ab60d882f24ecb754e2a59bb
Eduardo Casati <escasati@gmail.com>
2022-11-23 17:41:02 -0400

    Resolve o exercício final 4

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-4.js
@@ -0,0 +1,9 @@
+let number = 5;
+
+if (number  > 0) {
+    console.log("O número", number, "é positivo.");
+} else if (number < 0) {
+    console.log("O número", number, "é negativo.");
+} else {
+    console.log("O número é zero.");
+}
