93b5a6c185904f061c6db43774541566c16203f7
Eduardo Casati <escasati@gmail.com>
2022-11-23 19:21:51 -0400

    Resolve Exercício Final 8 Parte 3

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-8.parte3.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-8.parte3.js
@@ -1,5 +1,9 @@
 const cost = 100;
-const value = -1;
+const value = 230;
+
+let profit = value - (cost * 0.2 + cost);
+
+console.log(profit);
 
 if (cost < 0 || value < 0) {
     console.log("ERRO: valor(es) inválido(s)!");
