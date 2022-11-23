2631395c731af002421500327726a27c4287b79c
Eduardo Casati <escasati@gmail.com>
2022-11-23 18:08:52 -0400

    Resolve o switch do exercício final 6

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-6.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-6.js
@@ -1,5 +1,35 @@
 let chessPiece;
 
-
+chessPiece = "queen";
 
 console.log(chessPiece);
+
+switch (chessPiece) {
+    case "king":
+        console.log("The King moves from its square to a neighboring square.");
+        break;
+    
+    case "rook":
+        console.log("The Rook can move in its line or row.");
+        break;
+
+    case "bishop":
+        console.log("The Bishop moves diagonally.");
+        break;
+
+    case "queen":
+        console.log("The Queen may move like a Rook or a Bishop.");
+        break;
+
+    case "knight":
+        console.log("The Knight jumps in making the shortest move that is not a straight one.");
+        break;
+
+    case "pawn":
+        console.log("The Pawn moves one square straight ahead.");
+        break;
+
+    default:
+        console.log("ERROR!")
+        break;
+}
