8fdcbd31207c738a86108ef3c60fca8f77ed52a5
Eduardo Casati <escasati@gmail.com>
2022-11-23 17:21:09 -0400

    Resolve exercício final 1

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-1.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 3;
+
+console.log("A = 5, B = 3");
+console.log("Adição: ", a + b);
+console.log("Subtração :", a - b);
+console.log("Multiplicação: ", a * b);
+console.log("Divisão: ", a / b);
+console.log("Módulo: ", a % b);
