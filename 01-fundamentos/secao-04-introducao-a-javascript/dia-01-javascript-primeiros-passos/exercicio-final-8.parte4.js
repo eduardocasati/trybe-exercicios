cef62adad61b98341c4f2e5807864fef163ec035
Eduardo Casati <escasati@gmail.com>
2022-11-23 19:33:05 -0400

    Cria IF que define a aliquota do INSS

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-8.parte4.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-8.parte4.js
@@ -1,7 +1,21 @@
+let salarioBruto = 1600;
+let salarioLiquido;
+
 let aliquotaINSS;
 let aliquotaIR;
 
-let salarioBruto;
-let salarioLiquido;
+//if da aliquota do INNS
+if (salarioBruto > 0 && salarioBruto <= 1556.94) {
+    aliquotaINSS = 0.08;
+} else if (salarioBruto >= 1156.95 && salarioBruto <= 2594.92) {
+    aliquotaINSS = 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    aliquotaINSS = 0.11;
+} else if (salarioBruto >= 5189.82) {
+    aliquotaINSS = 570.88;
+} else {
+    console.log("Valor inválido!")
+}
 
-console.log(salarioLiquido);
+console.log(salarioBruto);
+console.log(aliquotaINSS);
