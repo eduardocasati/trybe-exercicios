8e20d41cb9a2f1ab2c470c4e4bd56cebb86f1798
Eduardo Casati <escasati@gmail.com>
2022-11-23 17:25:28 -0400

    Resolve o exercício final 2

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-final-2.js
@@ -0,0 +1,10 @@
+let number1 = 10;
+let number2 = 20;
+
+console.log("Número 1 =", number1, " e número 2 =", number2);
+
+if (number1 < number2) {
+    console.log("O número 1 é menor que o número 2");
+} else {
+    console.log("O número 2 é menor que o número 1");
+}
