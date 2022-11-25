83199ae707eb34f66807c1d0c46684076113283e
Eduardo Casati <escasati@gmail.com>
2022-11-25 17:16:26 -0400

    Resolve Exercício Final 1.2 do Dia 02 de Introdução à JavaScript

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-final-1.2.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+sum = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    sum = sum + numbers[index]
+}
+
+console.log(sum);
