0a6dc07bb5941d719f8ecc54399285d8ba617166
Eduardo Casati <escasati@gmail.com>
2022-11-25 16:19:15 -0400

    Cria lottterychecker.js e o enunciado do exercício

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/lotterychecker.js
@@ -0,0 +1,5 @@
+// Vamos desenvolver um algoritmo em JavaScript que verifica se a pessoa ganhou na Mega-Sena.
+// Para isso:
+// - A pessoa marca 6 números
+// - Uma sequência de 6 números é sorteada nesse mesmo intervalo.
+// - Se os números forem correspondentes.
