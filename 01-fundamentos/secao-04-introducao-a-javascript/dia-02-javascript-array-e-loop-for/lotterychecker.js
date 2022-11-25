f14f0d678f0d7f786a119c0dc2838f89a735b168
Eduardo Casati <escasati@gmail.com>
2022-11-25 16:50:23 -0400

    Cria o setup do exercício lotterychecker.js

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/lotterychecker.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/lotterychecker.js
@@ -3,3 +3,19 @@
 // - A pessoa marca 6 números
 // - Uma sequência de 6 números é sorteada nesse mesmo intervalo.
 // - Se os números forem correspondentes.
+
+// Jogo feito
+const gabrielGame = [8, 22, 26, 37, 42, 60];
+console.log('Números escolhidos:', gabrielGame);
+
+// Jogo Sorteado
+const megaSenaNumbers = [4, 20, 32, 28, 39, 60];
+console.log('Números sorteados:', megaSenaNumbers);
+
+// Variável que conta quantos acertos aconteceram
+let numberOfHits = 0;
+
+// Checa se os números são correspondentes
+
+
+console.log('Número de acertos:', numberOfHits);
