7f46c839afb9a84f484af6b37f4b177ab9f22183
Eduardo Casati <escasati@gmail.com>
2022-11-29 19:39:42 -0400

    Resolve exercicio-bonus-2.js Modulo Fundamentos - Seção 4 - Dia 3

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-2.js
@@ -0,0 +1,24 @@
+// MINHA SOLUÇÃO:
+
+let n = 5;
+
+let symbol = '*';
+let inputLine = '';
+
+for (index = 0; index < n; index += 1) {
+    inputLine = symbol + inputLine;
+    console.log(inputLine);
+}
+
+// GABARITO:
+//
+//let size = 5;
+//let symbol = '*';
+//let inputLine = '';
+//
+//for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+//  console.log(inputLine);
+//  inputLine = inputLine + symbol;
+//};
+
+
